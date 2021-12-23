5b51fac3d4f0a8507715fd3e038d76614cd87e52
Junya-kobayashi <atsubeee.ngzk@gmail.com>
2021-12-24 00:10:45 +0900

    test unit on react testing library

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,24 +1,27 @@
-import React, {useState} from "react"
+import { FC, ReactElement, useState } from "react";
 
-const Counter = (): React.ReactElement => {
+export const plusOne = (val: number): number => {
+    if(val>=10) return 10;
+    return val + 1;
+}
 
-    const [countState, setCount] = useState(0)
+const minusOne = (val: number): number => {
+	if(val<=0) return 0;
+	return val - 1;
+}
 
-    const onIncrement = () => {
-        setCount(countState + 1);
-    }
-
-    const onDecrement = () => {
-        setCount(countState - 1);
-    }
-
-    return (
-        <>
-            <span data-testid="count-number">{countState}</span>
-            <button type="button" onClick={onIncrement} data-testid="increment-button">+</button>
-            <button type="button" onClick={onDecrement} data-testid="decrement-button">-</button>
-        </>
-    )
+const Counter: FC = (): ReactElement => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>ただいまのカウンタ: <span data-testid="count-number">{count}</span></div>
+      <button className="plusButton" onClick={() => {setCount(plusOne(count))}} data-testid="increment-button">+</button>
+      <button className="minusButton" onClick={() => {setCount(minusOne(count))}} data-testid="decrement-button">-</button>
+      <button className="clearButton" onClick={() => {setCount(0)}}>
+        CLR
+      </button>
+    </>
+  );
 };
- 
+
 export default Counter;
--- src/test/conter.test.tsx
+++ src/test/conter.test.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+
 import { cleanup, fireEvent, render } from "@testing-library/react";
-import Counter, {onIncrement} from "../components/counter"
+import Counter, {plusOne} from "../components/counter"
 
 afterEach(cleanup);
 
-// const sel = (id:string) => `[data-testid="${id}"]`
+test("plusOneのテスト", () => {
+  expect(plusOne(1)).toBe(2);
+  expect(plusOne(10)).toBe(10);
+}) 
+
 
 it('+ボタン, -ボタンが表示されているかのテスト', ()=> {
   const {queryByTestId, getByTestId} = render(
@@ -15,21 +19,47 @@ it('+ボタン, -ボタンが表示されているかのテスト', ()=> {
 
 })
 
+it('デフォルト値が0になるかどうか', () => {
+  const {queryByTestId, getByTestId} = render(
+    <Counter />,
+  );
+  /** incrementが呼ばれているかのテスト */
+  const countNumberStr :string | null | undefined = queryByTestId(/count-number/i)?.textContent
+  expect(countNumberStr).not.toBeNull();
+  expect(countNumberStr).not.toBeUndefined();
+  expect(queryByTestId(/count-number/i)?.textContent).toBe(String(0));
+});
+
+it('値が10になるときにテストの値が10になるかどうか', () => {
+  const {queryByTestId, getByTestId} = render(
+    <Counter />,
+  );
+  /** incrementが呼ばれているかのテスト */
+  const countNumberStr :string | null | undefined = queryByTestId(/count-number/i)?.textContent
+  expect(countNumberStr).not.toBeNull();
+  expect(countNumberStr).not.toBeUndefined();
+  for(var i = 0; i < 10; i++) {
+    fireEvent.click(getByTestId(/increment-button/i));
+  }
+  expect(queryByTestId(/count-number/i)?.textContent).toBe(String(10));
+});
 
 it('+ボタンをクリックしたら+1された数字が表示されるか', () => {
-    const {queryByTestId, getByTestId} = render(
-      <Counter />,
-    );
-    /** incrementが呼ばれているかのテスト */
-    const countNumberStr :string | null | undefined = queryByTestId(/count-number/i)?.textContent
-    expect(countNumberStr).not.toBeNull();
-    expect(countNumberStr).not.toBeUndefined();
+  const {queryByTestId, getByTestId} = render(
+    <Counter />,
+  );
+  /** incrementが呼ばれているかのテスト */
+  const countNumberStr :string | null | undefined = queryByTestId(/count-number/i)?.textContent
+  expect(countNumberStr).not.toBeNull();
+  expect(countNumberStr).not.toBeUndefined();
 
-    const countNumber : number = Number(countNumberStr)
-    fireEvent.click(getByTestId(/increment/i));
-    expect(queryByTestId(/count-number/i)?.textContent).toBe(String(countNumber+ 1));
+  const countNumber : number = Number(countNumberStr)
+  fireEvent.click(getByTestId(/increment/i));
+  console.log(queryByTestId(/count-number/i)?.textContent);
+  expect(queryByTestId(/count-number/i)?.textContent).toBe(String(countNumber+ 1));
 
-  });
+});
+
 
 // 表示されているものが数値である
 
